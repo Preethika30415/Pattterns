09adccc1f2090bb74fecf91a8b14a4b27a7df8c3
srinivas <srinivas@sushma>
2020-06-17 18:49:17 +0530

    pattern8

---
+++ PracticePatterns/src/com/practice/patterns/P8.java
@@ -0,0 +1,17 @@
+package com.practice.patterns;
+
+public class P8 
+{
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++) 
+		{
+			for(int j=n-i;j>0;j--) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
