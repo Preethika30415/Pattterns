1541d45695058e104e1b4fcb411bf198f177eb27
srinivas <srinivas@sushma>
2020-06-17 18:47:40 +0530

    pattern5

---
+++ PracticePatterns/src/com/practice/patterns/P5.java
@@ -0,0 +1,18 @@
+package com.practice.patterns;
+
+public class P5 
+{
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		for(int i=1;i<n;i++) 
+		{
+			for(int j=1;j<=i+1;j++) 
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
