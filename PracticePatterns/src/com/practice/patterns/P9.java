45ad9f829d434952fcb5fa0e2782ff17efd74d38
srinivas <srinivas@sushma>
2020-06-17 18:50:03 +0530

    pattern9

---
+++ PracticePatterns/src/com/practice/patterns/P9.java
@@ -0,0 +1,18 @@
+package com.practice.patterns;
+
+public class P9 
+{
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++) 
+		{
+			for(int j=n-i;j>0;j--) 
+			{
+				System.out.print(i+1+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
