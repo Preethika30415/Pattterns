01f960feb845bf3e3eab89a1823422e330c83633
srinivas <srinivas@sushma>
2020-06-17 18:51:30 +0530

    pattern12

---
+++ PracticePatterns/src/com/practice/patterns/P12.java
@@ -0,0 +1,17 @@
+package com.practice.patterns;
+
+public class P12 
+{
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++) 
+		{
+			for(int j=0;j<n-i;j++) 
+			{
+				System.out.print(n-j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
